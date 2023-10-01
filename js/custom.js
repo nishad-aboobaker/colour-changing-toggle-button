bf3440dcab1cf23de5ec1419f294307e8e590b3b
NISHAD PP <121476081+nishad-aboobaker@users.noreply.github.com>
2023-10-01 11:09:22 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,100 @@
+let flag = "0";
+        function change() {
+          if (flag === "0") {
+            $("#first").css({ "background-color": "black" });
+            $("#second").css({ "background-color": "white" });
+            $("#third").css({ "background-color": "black" });
+            $("#fourth").css({ "background-color": "black" });
+            $("#fifth").css({ "background-color": "black" });
+            $("#sixth").css({ "background-color": "black" });
+            $("#seventh").css({ "background-color": "black" });
+            $("#eighth").css({ "background-color": "black" });
+            $("#ninth").css({ "background-color": "black" });
+            $("#btn").css({ "background-color": "black", color: "white" });
+            flag = "1";
+          } else if(flag==="1") {
+            $("#first").css({ "background-color": "black" });
+            $("#second").css({ "background-color": "black" });
+            $("#third").css({ "background-color": "white" });
+            $("#fourth").css({ "background-color": "black" });
+            $("#fifth").css({ "background-color": "black" });
+            $("#sixth").css({ "background-color": "black" });
+            $("#seventh").css({ "background-color": "black" });
+            $("#eighth").css({ "background-color": "black" });
+            $("#ninth").css({ "background-color": "black" });
+            $("#btn").css({ "background-color": "white", color: "black" });
+            flag = "2";
+          }else if(flag==="2") {
+            $("#first").css({ "background-color": "black" });
+            $("#second").css({ "background-color": "black" });
+            $("#third").css({ "background-color": "black" });
+            $("#fourth").css({ "background-color": "white" });
+            $("#fifth").css({ "background-color": "black" });
+            $("#sixth").css({ "background-color": "black" });
+            $("#seventh").css({ "background-color": "black" });
+            $("#eighth").css({ "background-color": "black" });
+            $("#ninth").css({ "background-color": "black" });
+            $("#btn").css({ "background-color": "white", color: "black" });
+            flag = "3";
+          }else if(flag==="3") {
+            $("#first").css({ "background-color": "black" });
+            $("#second").css({ "background-color": "black" });
+            $("#third").css({ "background-color": "black" });
+            $("#fourth").css({ "background-color": "black" });
+            $("#fifth").css({ "background-color": "white" });
+            $("#sixth").css({ "background-color": "black" });
+            $("#seventh").css({ "background-color": "black" });
+            $("#eighth").css({ "background-color": "black" });
+            $("#ninth").css({ "background-color": "black" });
+            $("#btn").css({ "background-color": "white", color: "black" });
+            flag = "4";
+          }else if(flag==="4") {
+            $("#first").css({ "background-color": "black" });
+            $("#second").css({ "background-color": "black" });
+            $("#third").css({ "background-color": "black" });
+            $("#fourth").css({ "background-color": "black" });
+            $("#fifth").css({ "background-color": "black" });
+            $("#sixth").css({ "background-color": "white" });
+            $("#seventh").css({ "background-color": "black" });
+            $("#eighth").css({ "background-color": "black" });
+            $("#ninth").css({ "background-color": "black" });
+            $("#btn").css({ "background-color": "white", color: "black" });
+            flag = "5";
+          }else if(flag==="5") {
+            $("#first").css({ "background-color": "black" });
+            $("#second").css({ "background-color": "black" });
+            $("#third").css({ "background-color": "black" });
+            $("#fourth").css({ "background-color": "black" });
+            $("#fifth").css({ "background-color": "black" });
+            $("#sixth").css({ "background-color": "black" });
+            $("#seventh").css({ "background-color": "white" });
+            $("#eighth").css({ "background-color": "black" });
+            $("#ninth").css({ "background-color": "black" });
+            $("#btn").css({ "background-color": "white", color: "black" });
+            flag = "6";
+          }else if(flag==="6") {
+            $("#first").css({ "background-color": "black" });
+            $("#second").css({ "background-color": "black" });
+            $("#third").css({ "background-color": "black" });
+            $("#fourth").css({ "background-color": "black" });
+            $("#fifth").css({ "background-color": "black" });
+            $("#sixth").css({ "background-color": "black" });
+            $("#seventh").css({ "background-color": "black" });
+            $("#eighth").css({ "background-color": "white" });
+            $("#ninth").css({ "background-color": "black" });
+            $("#btn").css({ "background-color": "white", color: "black" });
+            flag = "7";
+          }else{
+            $("#first").css({ "background-color": "black" });
+            $("#second").css({ "background-color": "black" });
+            $("#third").css({ "background-color": "black" });
+            $("#fourth").css({ "background-color": "black" });
+            $("#fifth").css({ "background-color": "black" });
+            $("#sixth").css({ "background-color": "black" });
+            $("#seventh").css({ "background-color": "black" });
+            $("#eighth").css({ "background-color": "black" });
+            $("#ninth").css({ "background-color": "white" });
+            $("#btn").css({ "background-color": "black", color: "white" });
+            flag = "0";
+          }
+        }
